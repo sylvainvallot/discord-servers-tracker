5b32d9e4e3f9df594aaa1289c69592e041846ce5
sylvainvallot <29737056+sylvainvallot@users.noreply.github.com>
2023-10-18 20:25:06 +0200

    add icon to header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Head from 'next/head';
+
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Footer from '../components/Footer';
@@ -6,10 +8,20 @@ import CallToAction from '../components/CallToAction';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className="container mx-auto py-12">
-      <Component {...pageProps} />
-      <CallToAction/>
-      <Footer/>
+    <div>
+      <Head>
+        <title>Discord Servers Tracker</title>
+        <link rel="shortcut icon" href="/images/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png"/>
+        <link rel="manifest" href="/site.webmanifest"/>
+      </Head>
+      <div className="container mx-auto py-12">
+        <Component {...pageProps} />
+        <CallToAction/>
+        <Footer/>
+      </div>
     </div>);
 }
 
