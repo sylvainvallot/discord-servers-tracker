3acb836b6e3a077630e096a52e0b33c683016729
sylvainvallot <29737056+sylvainvallot@users.noreply.github.com>
2023-10-25 00:09:13 +0200

    add custom breakpoint for small screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'xs': '500px',
+      },
+    },
   },
   plugins: [],
 }
